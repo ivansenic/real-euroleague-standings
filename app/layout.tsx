2123c40d54446bc661163261b1df6f9d88ab1f32
Ivan Senic <ivan@plainlyvideos.com>
2026-02-28 15:59:09 +0100

    add bmac (#27)
    * add bmac
    
    * deffer

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,17 +14,19 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "EuroLeague Standings 2025/26",
-  description: "Includes known EuroLeague 2025/26 tiebreakers in the standings.",
+  description:
+    "Includes known EuroLeague 2025/26 tiebreakers in the standings.",
   keywords: ["euroleague", "basketball", "standings", "table"],
   openGraph: {
     title: "Real EuroLeague Standings",
-    description: "Includes known EuroLeague 2025/26 tiebreakers in the standings.",
+    description:
+      "Includes known EuroLeague 2025/26 tiebreakers in the standings.",
     images: [
       {
-        url: "https://euroleague-standings.com/images/open-graph.png"
-      }
-    ]
-  }
+        url: "https://euroleague-standings.com/images/open-graph.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -43,13 +45,29 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="Euroleague Standings" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Euroleague Standings"
+        />
         <link rel="apple-touch-icon" href="/images/euroleague.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <script
+          defer
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="euroleague.standings"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Buy me a coffee and support this website 🙏"
+          data-color="#f85500"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        ></script>
       </body>
     </html>
   );
