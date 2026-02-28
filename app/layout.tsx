fa1544c528b04f8ad0d90513b5d391fad2d9704a
Ivan Senic <ivan@plainlyvideos.com>
2026-02-28 13:26:09 +0100

    deffer

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,7 @@ export default function RootLayout({
       >
         {children}
         <script
+          defer
           data-name="BMC-Widget"
           data-cfasync="false"
           src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
