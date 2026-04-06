0d0738421d834395bb759b8d6a87327137bce29f
Ivan Senic <ivan@plainlyvideos.com>
2026-04-06 16:19:19 +0200

    update caching config (#35)

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,34 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=2592000'
+            value: 'public, max-age=31536000, stale-while-revalidate=86400'
+          },
+        ],
+      },
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=604800, stale-while-revalidate=86400'
+          },
+        ],
+      },
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, stale-while-revalidate=86400'
+          },
+        ],
+      },
+      {
+        source: '/:all*.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
           },
         ],
       },
